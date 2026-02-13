9f5c13ed469608a5cd98192c3e61297f008887a9
dorlo1994 <dor.lotan94@gmail.com>
2026-02-13 17:52:07 +0200

    New homepage

--- src/App.jsx
+++ src/App.jsx
@@ -9,10 +9,10 @@ const md = new MarkdownIt();
 
 function UnderConstruction() {
   return (
-  <>
-    <h2 className="text-2xl text-red-600 dark:text-red-400 font-bold">Page Under Construction!</h2>
+  <div className="prose prose-invert mx-auto p-6">
+    <h1 className="text-red-600 dark:text-red-400">Page Under Construction!</h1>
     <p> Come back here later and I might just have something for you</p>
-   </>
+   </div>
    );
 
 }
@@ -113,22 +113,22 @@ function Blog() {
 
 function About() {
     return (
-      <article className="prose prose-invert">
-        <h2 className="text-2xl font-bold underline">About me</h2>
+      <article className="prose prose-invert mx-auto p-6">
+        <h1>About me</h1>
         <p>As someone who is deeply invested in the concept of digital agency, I am passionate about empowering individuals to take control of their own data and habits. With a strong background in personal data analysis, I have developed systems that enable me to collect and review my daily activities, projects, and progress in an automated manner. This allows me to gain valuable insights into how my habits and activities affect my productivity, ultimately leading to improved efficiency and effectiveness.</p>
 
 <p>As a proponent of digital agency, I believe it is essential for individuals to be able to participate in the conversation around data collection and usage. Unfortunately, many people are digitally illiterate, which puts them at a significant disadvantage in today's world. By sharing my experiences and thoughts on this topic, I hope to help bridge this gap and promote a more informed and empowered community.</p>
 
 <p>In my quest for digital agency, I have come to realize the importance of automation as an empowering tool. By leveraging technology to automate repetitive tasks and processes, I am able to free up time and mental energy to focus on higher-level thinking and strategy.</p>
 
-<p>As a thought leader in this space, I am committed to exploring the intersection of digital agency and algorithmic influence. I believe that it is essential for individuals to have control over their own data and habits, rather than relying on algorithms to make decisions for them.</p>
+<p>As a thinker in this space, I am committed to exploring the intersection of digital agency and algorithmic influence. I believe that it is essential for individuals to have control over their own data and habits, rather than relying on algorithms to make decisions for them.</p>
       </article>);
 }
 
 function Contact() {
     return (
-      <article className="prose prose-invert">
-      <h2 className="text-2xl font-bold underline">Contact me</h2>
+      <article className="prose prose-invert mx-auto p-6">
+      <h1>Contact Me</h1>
       <ul>
         <li key="email">
           <a href="mailto:dor.lotan94@gmail.com">Email</a>
