a5c68ec007d011094cee2169ad900b7a862b6e12
dorlo1994 <dor.lotan94@gmail.com>
2026-03-10 01:23:48 +0200

    Fix

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/personal-website/'
   plugins: [react(), tailwindcss()],
   esbuild: {
     loader: 'jsx', // ensure jsx files are correctly parsed
